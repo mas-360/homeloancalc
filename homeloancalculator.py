f559ce15fcebd1e9f1a5c6bb1d9e8a3ce1210914
masinsight <126551615+mas-360@users.noreply.github.com>
2023-09-14 20:41:53 +0200

    Update homeloancalculator.py

--- homeloancalculator.py
+++ homeloancalculator.py
@@ -203,8 +203,8 @@ def generate_amortization_schedule(loan_amount, new_interest_rate, new_loan_term
 
     for month in range(1, num_payments + 1):
         interest_payment = remaining_balance * monthly_interest_rate 
-        principal_payment = monthly_payment - interest_payment - new_extra_payment
-        remaining_balance -= principal_payment
+        principal_payment = monthly_payment - interest_payment 
+        remaining_balance -= principal_payment - new_extra_payment
 
         amortization_schedule.append({
             'Month': month,
